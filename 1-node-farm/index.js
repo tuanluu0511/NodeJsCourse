c1f95ef07ab06fed00ab266f68d7db28e4ed81bd
Tuan Luu <tuanluu.hcmc@gmail.com>
2022-04-07 16:19:43 +0700

    Create a simple web server

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,9 +1,36 @@
 const fs = require('fs');
+const http = require('http');
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+/////////////////////////////////////
+//SERVER
+const server = http.createServer((req, res) => {
+  res.end('Hello from the server');
+});
 
-const textOut = `This is what we know about avocado ${textIn}. \nCreated on ${Date.now()}`;
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000');
+});
 
-fs.writeFileSync('./txt/output.txt', textOut);
+/////////////////////////////////////
+// FILE
+//Blocking, synchronous way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 
-console.log('File Written');
+// const textOut = `This is what we know about avocado ${textIn}. \nCreated on ${Date.now()}`;
+
+// fs.writeFileSync('./txt/output.txt', textOut);
+
+// console.log('File Written');
+
+// Non-blocking, asynchronous way
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//   if (err) console.log('There is an error!!!');
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2);
+//     fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, (err) => {
+//         console.log('File Written!');
+//       });
+//     });
+//   });
+// });
